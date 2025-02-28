ea1fc0ce280641e43d9679a33481dc388b356cb1
CaciaRodrigues <caciarodriguues@gmail.com>
2025-02-28 15:55:52 -0300

    Componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/header/Header"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-9xl text-blue-primary">Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-screen h-16 bg-white-ntl shadow-lg'>
+      <img src="https://ik.imagekit.io/caciasrs/teste-pratico-frontend/Logo.svg?updatedAt=1740759169088" alt="BeTalent Logo" className='w-24 h-full p-2.5 lg:ml-5 ml-2.5 sticky'/>
+    </div>
+  )
+}
+
+export default Header
