358c05c020bc4a89086a06a7cfbdab546943f897
CaciaRodrigues <caciarodriguues@gmail.com>
2025-03-03 10:58:47 -0300

    Ajustes na tabela

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -13,7 +13,7 @@ const Table = () => {
 
   return (
     <>
-      <div className="flex gap-4 items-center w-[90%]">
+      <div className="flex gap-4 items-center">
       <h2 className="text-xl font-semibold">Funcionários</h2>
       <div className="flex items-center gap-2 border border-gray-10 rounded-xl w-[287px] h-[48px] px-4 py-3 bg-white-ntl">
         {/* <label className="text-gray-20">Pesquisa</label> */}
@@ -26,43 +26,45 @@ const Table = () => {
       </div>
     </div>
 
-      <div className="w-screen h-screen container mx-auto p-4 flex justify-center items-center">
-        <table className="w-[90%] text-left rounded-lg overflow-hidden shadow-lg">
-          <thead>
-            <tr className="bg-blue-primary text-white-ntl h-12">
-              <th className=" pl-8 text-left">FOTO</th>
-              <th className=" text-left">NOME</th>
-              <th className=" text-left">CARGO</th>
-              <th className=" text-left">DATA DE ADMISSÃO</th>
-              <th className=" text-left">TELEFONE</th>
-            </tr>
-          </thead>
-          <tbody className="bg-white text-black-ntl grid-cols-1 font-[500]">
-            {employees.map((employee) => (
-              <tr
-                key={employee.id}
-                className="h-12 border-b border-gray-10 hover:bg-gray-00"
-              >
-                <td className="pl-8 py-1.5 flex items-center">
-                  <img
-                    src={employee.image}
-                    alt={employee.name}
-                    className="size-9 rounded-full object-cover"
-                  />
-                </td>
-                <td className=" ">{employee.name}</td>
-                <td className=" ">{employee.job}</td>
-                <td className="">
-                  {new Date(employee.admission_date).toLocaleDateString(
-                    "pt-BR"
-                  )}
-                </td>
-                <td className=" ">{formatPhoneNumber(employee.phone)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+    <div className="w-screen h-screen container mx-auto p-4 flex justify-center items-center">
+  <table className="w-[75%] table-fixed text-left rounded-lg overflow-hidden shadow-lg">
+    <thead>
+      <tr className="bg-blue-primary text-white-ntl h-12">
+        <th className="w-1/5 pl-8">FOTO</th>
+        <th className="w-1/5">NOME</th>
+        <th className="w-1/5">CARGO</th>
+        <th className="w-1/5">DATA DE ADMISSÃO</th>
+        <th className="w-1/5">TELEFONE</th>
+      </tr>
+    </thead>
+    <tbody className="bg-white text-black-ntl font-[500]">
+      {employees.map((employee) => (
+        <tr
+          key={employee.id}
+          className="h-12 border-b border-gray-10 hover:bg-gray-00"
+        >
+          <td className="w-1/5 pl-8 py-1.5">
+            <div className="flex justify-start">
+              <img
+                src={employee.image}
+                alt={employee.name}
+                className="size-9 rounded-full object-cover"
+              />
+            </div>
+          </td>
+          <td className="w-1/5">{employee.name}</td>
+          <td className="w-1/5">{employee.job}</td>
+          <td className="w-1/5">
+            {new Date(employee.admission_date).toLocaleDateString("pt-BR")}
+          </td>
+          <td className="w-1/5">{formatPhoneNumber(employee.phone)}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
+
     </>
   );
 };
